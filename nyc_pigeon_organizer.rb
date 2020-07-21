e3db18ad8de4e08e07bdd0d573060ef52cafa986
Justin Webb <webb.n.justin@gmail.com>
2020-07-21 18:35:54 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+ results = data.each_with_object({}) do |(key, value), final|
+    value.each do |inner, names|
+      names.each do |name|
+        if !final[name]
+          final[name] = {}
+        end
+        
+        if !final[name][key]
+          !final[name][key] = []
+        end
+        final[name][key].push(inner.to_s)
+      end 
+    end
+ 
+  end
+end 
