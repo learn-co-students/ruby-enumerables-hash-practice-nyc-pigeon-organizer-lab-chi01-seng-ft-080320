3b71b67eb5d0deb4e157e24346f583ca401d50aa
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-27 00:27:23 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object({}) do |(k, v), new_array|
+
+  v.each do |inner_k, names| 
+    
+    names.each do |name|
+      if !new_array[name]
+        new_array[name] = {}
+      end
+      if !new_array[name][k]
+        !new_array[name][k] = []
+      end
+      new_array[name][k].push(inner_k.to_s)
+      end
+    end
+  end
 end
