0d29e72a5f8dfa3a868ca7c224fc8ac2b4d32471
lindsayMecher <lindsaymecher@gmail.com>
2020-09-08 18:21:59 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -20,22 +20,20 @@ pigeon_data = {
 }
 
 def nyc_pigeon_organizer(data)
-  new_hash = {}
-  data.each do |key, value|
-    value.each do |new_value, names|
-      names.each do |name|
+  results = data.each_with_object({}) do |(level_one_data, level_two_data), new_hash|
+    level_two_data.each do |level_three_data, p_names|
+      p_names.each do |name|
         if !new_hash[name]
           new_hash[name] = {}
+        end 
+        if !new_hash[name][level_one_data]
+          new_hash[name][level_one_data] = []
         end
-
-        if !new_hash[name][key]
-          new_hash[name][key] = []
-        end
-        new_hash[name][key] << new_value.to_s
+        new_hash[name][level_one_data] << level_three_data.to_s
       end
     end
   end
-  new_hash
+  # p results
 end
 
 nyc_pigeon_organizer(pigeon_data)
